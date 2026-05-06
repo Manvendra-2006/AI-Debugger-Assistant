e3d3e47ccd44d1432e77d53f06d13d41b9ea7374
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-05-06 12:57:25 +0530

    build : add build files

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,10 @@ import authRouter from './routes/authRoutes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import debugRouter from './routes/debugRoutes.js'
+import path from 'path'
 const app = express()
+const _dirname = path.resolve()
+
 app.use(cookieParser())
 app.use(cors({
     origin:"http://localhost:5173",
@@ -12,4 +15,8 @@ app.use(cors({
 app.use(express.json())
 app.use("/api/auth",authRouter)
 app.use("/api/debug",debugRouter)
+app.use(express.static(path.join(_dirname,"/Frontend/Debugger/dist")))
+app.get('*',(req,resp)=>{
+    resp.sendFile(path.resolve(_dirname,"Frontend","Debugger","dist","index.html"))
+})
 export default app
