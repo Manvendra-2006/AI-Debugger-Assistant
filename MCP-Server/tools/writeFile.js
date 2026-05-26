d01cbf274acea9d1e683478fff8b78d274bc7306
Manvendra Bhardwaj <manvendrabhardwaj8@gmail.com>
2026-05-26 09:47:19 +0530

    Update writeFile.js

--- MCP-Server/tools/writeFile.js
+++ MCP-Server/tools/writeFile.js
@@ -1,32 +1,21 @@
-import fs from 'fs'
+import fs from "fs"
 
-export const writeFile = (filepath,content) => {
+export default async function writeFile(
+    filepath,
+    content
+) {
+
     try {
 
-        fs.writeFileSync( //  ye code main new content ko update karega 
-            filepath,content,'utf-8'
+        fs.writeFileSync(
+            filepath,
+            content
         )
-        return {  // har tool ka return format structured response object hota hain 
-            content:[
-                {
-                    type:'text', // ye tool textual format main response return karta hain 
-                    text:
-                    `File updated successfully`
-                }
-            ]
-        }
 
-    } catch(error){
+        return "File updated successfully"
 
-        return {
-            content:[
-                {
-                    type:'text',
-                    text:error.message
-                }
-            ]
-        }
+    } catch (err) {
+
+        return err.message
     }
 }
-
-// This tool is used for writing updated code in a file 
