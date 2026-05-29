cf02129b72223012fe44c47847544b70527b5fff
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-05-29 10:40:25 +0530

    fix:cors issue

--- Backend/app.js
+++ Backend/app.js
@@ -3,22 +3,18 @@ import authRouter from './routes/authRoutes.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import debugRouter from './routes/debugRoutes.js'
-import path from 'path'
 import fileRouter from './routes/fileroutes.js'
+
 const app = express()
-const _dirname = path.resolve()
 
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
 }))
 app.use(express.json())
-app.use("/api/auth",authRouter)
-app.use("/api/debug",debugRouter)
-app.use("/api/files",fileRouter)
-app.use(express.static(path.join(_dirname,"/Frontend/Debugger/dist")))
-app.get('/*splat',(req,resp)=>{
-    resp.sendFile(path.resolve(_dirname,"Frontend","Debugger","dist","index.html"))
-})
+app.use("/api/auth", authRouter)
+app.use("/api/debug", debugRouter)
+app.use("/api/files", fileRouter)
+
 export default app
